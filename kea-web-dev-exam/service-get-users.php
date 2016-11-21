2bd0b48d0649a1ce7cf88509ebd6e6a81a9851ba
Danny Olsen <danny.hansen@me.com>
2016-11-21 18:22:01 +0100

    Chat ==
    1. forsøg på at compare, om der er nye values i chatlisten

--- kea-web-dev-exam/service-get-users.php
+++ kea-web-dev-exam/service-get-users.php
@@ -8,14 +8,17 @@
 	// $jUser->userId = $user;
 	// $jUser->message = $message
 	//$user = $decodeUsers[]->id;
-	$jUser = json_decode('{}');
-	for ($i = 0; $i < 3; $i++) {
+	$testArray = [];
+	
+	for ($i = 0; $i < Count($decodeUsers); $i++) {
+		$jUser = json_decode('{}');
 		$jUser->id = $decodeUsers[$i]->id;
 		$jUser->userEmail = $decodeUsers[$i]->userEmail;
 		//	echo $jUser;
-		
+		array_push($testArray, $jUser);
 	}
-$test = json_encode($jUser);
-echo $test;
+
+	$test = json_encode($testArray);
+	echo $test;
 	
  ?>
